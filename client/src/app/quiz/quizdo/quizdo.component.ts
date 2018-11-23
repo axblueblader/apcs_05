9fc0695f71f9fb5bb575b0a8a7b93e65285a958f
Nghi Pham <ptnghi@apcs.vn>
2018-11-23 18:58:03 +0700

    page swap, result highlight

--- client/src/app/quiz/quizdo/quizdo.component.ts
+++ client/src/app/quiz/quizdo/quizdo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {QuizdataService} from '../quizdata.service';
 
 @Component({
   selector: 'app-quizdo',
@@ -6,13 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizdo.component.css']
 })
 export class QuizdoComponent implements OnInit {
+  private quizList;
+  private currQuest: number;
+  @Output() private changeState = new EventEmitter<void>();
 
-  constructor() { }
+  constructor(private quizDataService: QuizdataService) { }
 
   ngOnInit() {
+    this.quizList  = this.quizDataService.getList();
+    this.currQuest = 0 ;
+    console.log(this.quizList.length);
   }
 
   imgClick(color: string) {
     console.log(color);
+    console.log(this.currQuest);
+    if (this.currQuest < (this.quizList.length - 1)) {
+      this.currQuest++;
+      this.quizDataService.addResult(color);
+    } else {
+      this.changeState.emit();
+    }
+
   }
 }
