b40c8b2929a0768d6581e0c33a6681c3cb860d7d
Nghi Pham <ptnghi@apcs.vn>
2018-12-04 23:06:32 +0700

    Chat barebone

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
 
+
+
+export class ChatComponent implements OnInit {
+  @ViewChild('msgbox') msgbox: ElementRef;
+  msg_list: string[] = [];
   constructor() { }
 
   ngOnInit() {
   }
 
+  addMsg(value: string) {
+    this.msg_list.push(value);
+    this.msgbox.nativeElement.scrollTop = this.msgbox.nativeElement.scrollHeight;
+    console.log(this.msgbox.nativeElement);
+  }
 }
+
+
