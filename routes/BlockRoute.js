48040a0f2b928fe33739dd5000c5f46917baa78b
thanhtan <thanhtan@thanhtans-MacBook-Pro.local>
2018-12-06 17:29:38 +0700

    Block and Unblock

--- routes/BlockRoute.js
+++ routes/BlockRoute.js
@@ -2,6 +2,6 @@ const express=require('express');
 let route=express.Router();
 const controllers=require('../controllers/Blockcontrollers')
 const UserMiddlewares=require('../middlewares/UserMiddlewares')
-route.post('/block',controllers.BlockUser);
-route.put('/unblock',controllers.UnblockUser);
+route.post('/block',UserMiddlewares.AlreadySignedIn,controllers.BlockUser);
+route.put('/unblock',UserMiddlewares.AlreadySignedIn,controllers.UnblockUser);
 module.exports=route;
